c10e60e9d66934c057cc5a4dd9edd4b4680c02af
kajalkapadiya <kajalkapadiya18@gmail.com>
2024-04-28 18:03:12 +0530

    email sent sucessfully for forgot pass

--- controllers/forgotPass.js
+++ controllers/forgotPass.js
@@ -1,15 +1,15 @@
 const nodemailer = require("nodemailer");
+require("dotenv").config();
 
 const forgotPass = (req, res) => {
   const { email } = req.body;
-  console.log(email);
   const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
     port: 587,
     secure: false, // Use `true` for port 465, `false` for all other ports
     auth: {
       user: "kajalkapadiya18@gmail.com",
-      pass: "xsmtpsib-4b84dd2608d0767cd300ba898bca4948b93df3f2bc2db2aeb590b9544b94a39f-GUfrm9ODkhaYHcsF",
+      pass: process.env.API_KEY,
     },
   });
 
