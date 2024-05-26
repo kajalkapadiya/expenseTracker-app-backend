d00b63aeacab166275e14c642b01dac562e033cd
kajalkapadiya <kajalkapadiya18@gmail.com>
2024-05-26 10:37:26 +0530

    solved commits error

--- models/index.js
+++ models/index.js
@@ -1,16 +1,11 @@
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
-const sequelize = new Sequelize(
-  "expenseTracker",
-  "root",
-  process.env.SEQ_PASS,
-  {
-    host: "localhost",
-    logging: false,
-    dialect: "mysql",
-  }
-);
+const sequelize = new Sequelize("expenseTracker", "root", "12345678", {
+  host: "localhost",
+  logging: false,
+  dialect: "mysql",
+});
 
 try {
   sequelize.authenticate();
